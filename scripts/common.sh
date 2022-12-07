00c800e4ff04ca55829296385dd630cda444775f
Maciej Małecki <me@mmalecki.com>
2022-12-07 16:41:13 +0100

    Configure both networks with same settings

--- scripts/common.sh
+++ scripts/common.sh
@@ -6,16 +6,22 @@
 # 64-bit architecture. Remove the preloads.
 rm /etc/ld.so.preload
 
-# Enable mDNS, disable LLMNR on the network
-tee /etc/systemd/network/wlan0.network <<-EOF
-[Match]
-Name=wlan0
+# Use the same configurations for both networks, but keep them separate in case
+# they need to be re-configured separately.
+configure_network () {
+  tee /etc/systemd/network/$1.network <<-EOF
+    [Match]
+    Name=$1
 
-[Network]
-DHCP=yes
-MulticastDNS=yes
-LLMNR=no
-EOF
+    [Network]
+    DHCP=yes
+    MulticastDNS=yes
+    LLMNR=no
+  EOF
+}
+
+configure_network wlan0
+configure_network eth0
 
 # Enable mDNS, disable LLMNR on the resolver
 tee /etc/systemd/resolved.conf <<-EOF
