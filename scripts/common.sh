6cb15fd3531706614673dd2d39bd0e687320b488
Maciej Małecki <me@mmalecki.com>
2022-12-28 02:38:38 +0100

    Fix `configure_network` indentation
    Remove indented HEREDOC, they only allow for tabs.

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,15 +9,15 @@ rm /etc/ld.so.preload
 # Use the same configurations for both networks, but keep them separate in case
 # they need to be re-configured separately.
 configure_network () {
-  tee /etc/systemd/network/$1.network <<-EOF
-    [Match]
-    Name=$1
+  tee /etc/systemd/network/$1.network <<EOF
+[Match]
+Name=$1
 
-    [Network]
-    DHCP=yes
-    MulticastDNS=yes
-    LLMNR=no
-  EOF
+[Network]
+DHCP=yes
+MulticastDNS=yes
+LLMNR=no
+EOF
 }
 
 configure_network wlan0
