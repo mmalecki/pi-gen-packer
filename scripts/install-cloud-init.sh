a1f5f8961c8f5920a020d8011fef501a3ee196f6
Maciej Małecki <me@mmalecki.com>
2023-12-08 01:49:55 +0100

    Disable `cloud-init` network configuration

--- scripts/install-cloud-init.sh
+++ scripts/install-cloud-init.sh
@@ -9,3 +9,9 @@ datasource:
   NoCloud:
     fs_label: None
 EOF
+
+# We're fairly certain about what the network hardware configuration is going
+# to look like, so let's disable cloudinit's.
+cat > /etc/cloud/cloud.cfg.d/disable-network-configuration.cfg <<EOF
+network: { config: disabled }
+EOF
