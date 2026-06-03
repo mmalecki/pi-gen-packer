51ea3956c809b256dbf58c5b55ddf7e779812e64
Maciej Małecki <me@mmalecki.com>
2026-06-04 00:44:30 +0200

    Enable recording in Rapiscope

--- scripts/install-rapiscope.sh
+++ scripts/install-rapiscope.sh
@@ -6,7 +6,7 @@ tee /etc/systemd/user/rapiscope.service <<-EOF
 Description=Rapiscope preview
 
 [Service]
-ExecStart=libcamera-vid -t 0 --hflip --vflip
+ExecStart=libcamera-vid --signal --initial pause --inline --split -o 'recordings/%%03d.h264' -t 0 --hflip --vflip
 
 [Install]
 WantedBy=default.target
@@ -16,6 +16,13 @@ tee /etc/systemd/logind.conf <<-EOF
 [Login]
 NAutoVTs=6
 EOF
+
+tee /usr/local/bin/toggle-record <<-EOF
+#!/usr/bin/env bash
+kill -SIGUSR1 $$(pidof libcamera-vid)
+EOF
+
+chmod +x /usr/local/bin/toggle-record
 
 tee /etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
 [Service]
